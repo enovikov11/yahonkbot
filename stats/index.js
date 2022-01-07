6865814e0644ecc555302206f504b03ead22662e
Evgenii Novikov <enovikov11@yandex.ru>
2022-01-07 13:38:56 +0300

    stats@1.1.0 чтение лога yahonkbot

--- stats/index.js
+++ stats/index.js
@@ -1,4 +1,6 @@
-const puppeteer = require('puppeteer'), fs = require('fs'), html = fs.readFileSync('./render.html', 'utf-8');
+const puppeteer = require('puppeteer'), fs = require('fs'), html = fs.readFileSync('./render.html', 'utf-8'),
+    exportedTitlesFilepath = '/titles-from-export/titles.json',
+    updatesLogFilepath = '/logs/updates.log';
 
 function render(messages) {
     function escapeHtml(html) {
@@ -43,14 +45,41 @@ function getExistingIds() {
     return (dir.join(',').match(/\d+/g) || []).map(n => +n);
 }
 
-function filter(messages) {
-    return messages
+function getChatTitle(update) { return update && update.message && update.message.new_chat_title ? update.message.new_chat_title : null; }
+function getChatId(update) { return update && update.message && update.message.chat && update.message.chat.id ? update.message.chat.id : null }
+function getDate(update) { return update && update.message && update.message.date ? update.message.date : null }
+function getFirstName(update) { return update && update.message && update.message.from && update.message.from && update.message.from.first_name ? update.message.from.first_name : null }
+function getLastName(update) { return update && update.message && update.message.from && update.message.from && update.message.from.last_name ? update.message.from.last_name : null }
+
+function readTitlesHistory() {
+    const exportedTitles = JSON.parse(fs.readFileSync(exportedTitlesFilepath, 'utf-8'))
         .filter(({ action }) => action === 'edit_group_title')
+        .map(({ date, title, actor }) => ({ date, title, actor }));
+
+    const titlesFromUpdates = fs.readFileSync(updatesLogFilepath, 'utf-8').split('\n')
+        .map(line => {
+            try {
+                return JSON.parse(line);
+            } catch (e) {
+                return {};
+            }
+        })
+        .filter(({ update }) => getChatTitle(update) && String(getChatId(update)) === process.env.STATS_CHAT_ID)
+        .map(({ update }) => ({
+            date: new Date(getDate(update) * 1000).toISOString().substr(0, 19),
+            title: getChatTitle(update),
+            actor: getFirstName(update) + ' ' + getLastName(update)
+        }));
+
+    const mergedTitles = [...exportedTitles, ...titlesFromUpdates]
+        .filter(({ date, title, actor }) => date && title && actor)
         .filter(({ title }, i, array) => i === 0 || (array[i - 1].title !== title));
+
+    return mergedTitles;
 }
 
 async function makeImages() {
-    let messages = filter(JSON.parse(fs.readFileSync('/titles-from-export/titles.json', 'utf-8'))),
+    let messages = readTitlesHistory(),
         i = 1,
         result = { usedCards: 0 };
     const browser = await puppeteer.launch({ args: ['--no-sandbox'] }), ids = getExistingIds();
@@ -62,12 +91,13 @@ async function makeImages() {
         await page.goto('data:text/html,' + html, { waitUntil: 'networkidle2' });
 
         result = await page.evaluate(render, messages);
+        console.log({ result, i });
 
         if (result.usedCards) {
             messages = messages.splice(result.usedCards);
         }
 
-        if (!ids.includes(i) && messages.length > 0 && result.usedCards) {
+        if (process.env.DEBUG_MAKE_ALL_IMAGES === 'true' || (!ids.includes(i) && messages.length > 0 && result.usedCards)) {
             await page.screenshot({ path: `/results/${i}.png` });
         }
 
