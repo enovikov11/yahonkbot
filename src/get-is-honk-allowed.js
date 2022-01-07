4b06437861787e81e526422273166a75fcbe8568
Evgenii Novikov <enovikov11@yandex.ru>
2022-01-07 19:58:37 +0300

    @2.4.0 honk premium

---
+++ src/get-is-honk-allowed.js
@@ -0,0 +1,53 @@
+const { api } = require('./api'), lastHonkAtChat = {}, advertisedChats = new Set();
+
+async function getIsHonkAllowed(update) {
+    if (update?.message?.chat?.type === 'private') {
+        return true;
+    }
+
+    if (process.env.DEBUG_DISABLE_COOLDOWN === 'true') {
+        return true;
+    }
+
+    const timeAfterHonk = Date.now() - (lastHonkAtChat[update.message.chat.id] || 0);
+
+    if (timeAfterHonk > 5 * 60000) {
+        lastHonkAtChat[update.message.chat.id] = Date.now();
+
+        advertisedChats.delete(update.message.chat.id);
+
+        return true;
+    }
+
+    const chatMember = await api('getChatMember', {
+        chat_id: Number(process.env.PREMIUM_CHAT_ID),
+        user_id: update.message.from.id
+    }), isPremium = chatMember?.result?.user?.id === update.message.from.id && update.message.from.id !== Number(process.env.PREMIUM_SERVICE_USER_ID);
+
+    if (isPremium) {
+        if (timeAfterHonk > 2 * 60000) {
+            lastHonkAtChat[update.message.chat.id] = Date.now();
+
+            advertisedChats.delete(update.message.chat.id);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (!advertisedChats.has(update.message.chat.id)) {
+        advertisedChats.add(update.message.chat.id);
+
+        await api('sendMessage', {
+            chat_id: update.message.chat.id,
+            text: `С honk premium можно пускать гусей каждые 2 минуты. Жертвуешь на благотворительность 500 рублей в месяц? Пиши t.me/enovikov11 чтобы получить honk premium`,
+            disable_web_page_preview: true,
+            reply_to_message_id: update?.message?.message_id
+        });
+    }
+
+    return false;
+}
+
+module.exports = getIsHonkAllowed;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const advertiseContributing = require('./advertise-contributing'), doHonk = require('./do-honk'),
-    isHonkAllowed = require('./is-honk-allowed'), log = require('./log'), doLoop = require('./do-loop'),
+    getIsHonkAllowed = require('./get-is-honk-allowed'), log = require('./log'), doLoop = require('./do-loop'),
     anounceCommands = require('./anounce-commands'), sendStats = require('./send-stats');
 
 async function processUpdate(update) {
-    const isAdvertised = await advertiseContributing(update);
-    const isSentStats = await sendStats(update);
+    const isAdvertised = await advertiseContributing(update), isSentStats = await sendStats(update);
+    let isHonkSuccessful = false;
 
-    const isHonkSuccessful = !isAdvertised && !isSentStats && isHonkAllowed(update);
-
-    if (isHonkSuccessful) {
-        await doHonk(update);
+    if (!isAdvertised && !isSentStats) {
+        isHonkSuccessful = await getIsHonkAllowed(update);
+        if (isHonkSuccessful) {
+            await doHonk(update);
+        }
     }
 
     log({ update, isHonkSuccessful });
