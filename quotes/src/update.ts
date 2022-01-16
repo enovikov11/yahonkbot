01882d228963e574be669bb70fe58f46f1b1fa86
Evgenii Novikov <enovikov11@yandex.ru>
2022-01-16 22:49:48 +0300

    quotes: ATH/ATL

--- quotes/src/update.ts
+++ quotes/src/update.ts
@@ -1,9 +1,6 @@
 import { getChatTitle, setChatTitle } from "./telegram.ts";
 import { getPrice } from "./yahoo.ts";
 
-const chatId = Number(Deno.env.get("QUOTES_CHAT_ID")),
-  ticker = Deno.env.get("QUOTES_TICKER") || "";
-
 export function makeNewTitle(oldTitle: string, price: number): string | null {
   const candidates = [...oldTitle.matchAll(/\b\d+\.\d{2}\b/g)]
     .map((n) => +n)
@@ -11,12 +8,36 @@ export function makeNewTitle(oldTitle: string, price: number): string | null {
       Math.max(candidate / price, price / candidate) < 1.3
     );
 
-  return candidates.length === 1
-    ? oldTitle.replace(candidates[0].toFixed(2), price.toFixed(2))
-    : null;
+  if (candidates.length !== 1) {
+    return null;
+  }
+
+  const candidate = candidates[0],
+    trenders = [...oldTitle.matchAll(/(AT|Y|M|W|D)(H|L)\b/g)];
+
+  if (trenders.length > 1) {
+    return null;
+  }
+
+  if (trenders.length === 1) {
+    if (trenders[0][2] === "L" && candidate < price) {
+      return null;
+    }
+
+    if (trenders[0][2] === "H" && candidate > price) {
+      return null;
+    }
+  }
+
+  const newTitle = oldTitle.replace(candidate.toFixed(2), price.toFixed(2));
+
+  return newTitle === oldTitle ? null : newTitle;
 }
 
 export async function update() {
+  const chatId = Number(Deno.env.get("QUOTES_CHAT_ID")),
+    ticker = Deno.env.get("QUOTES_TICKER") || "";
+
   const oldTitle: string = await getChatTitle(chatId),
     price: number = await getPrice(ticker),
     newTitle: string | null = makeNewTitle(oldTitle, price);
--- quotes/test/titles.test.ts
+++ quotes/test/titles.test.ts
@@ -1,16 +1,37 @@
 import { makeNewTitle } from "../src/update.ts";
 import { assertEquals } from "https://deno.land/std@0.121.0/testing/asserts.ts";
 
-Deno.test("Смена названия", () => {
-  assertEquals(makeNewTitle("Типо чатик 12.34", 13.24), "Типо чатик 13.24");
-  assertEquals(makeNewTitle("Типо чатик 12.30", 13.24), "Типо чатик 13.24");
-  assertEquals(makeNewTitle("Типо чатик 12.30", 130.24), null);
-  assertEquals(makeNewTitle("Типо чатик 12.34 12.30", 13.24), null);
-  assertEquals(
-    makeNewTitle("Типо чатик 12.34 5678", 13.24),
-    "Типо чатик 13.24 5678",
-  );
-  assertEquals(makeNewTitle("Типо чатик 12.34", 13), "Типо чатик 13.00");
-  assertEquals(makeNewTitle("Типо чатик 14.00", 13), "Типо чатик 13.00");
-  assertEquals(makeNewTitle("Типо чатик (12.34)", 13.24), "Типо чатик (13.24)");
+const cases = [
+  { oldTitle: "Чат 11.11", price: 11.11, newTitle: null },
+  { oldTitle: "Чат 11.11", price: 110.12, newTitle: null },
+  { oldTitle: "Чат 11.11 11.11", price: 11.12, newTitle: null },
+
+  { oldTitle: "Чат 11.11", price: 11.12, newTitle: "Чат 11.12" },
+  { oldTitle: "Чат 11.13", price: 11.12, newTitle: "Чат 11.12" },
+  { oldTitle: "Чат (11.11)", price: 11.12, newTitle: "Чат (11.12)" },
+
+  { oldTitle: "Чат 11.11", price: 11, newTitle: "Чат 11.00" },
+  { oldTitle: "Чат 11.00", price: 12, newTitle: "Чат 12.00" },
+
+  { oldTitle: "Чат 11.11 9999", price: 11.12, newTitle: "Чат 11.12 9999" },
+
+  { oldTitle: "Чат ATH 11.11", price: 11.12, newTitle: "Чат ATH 11.12" },
+  { oldTitle: "Чат ATH 11.13", price: 11.12, newTitle: null },
+
+  { oldTitle: "Чат ATL 11.11", price: 11.12, newTitle: null },
+  { oldTitle: "Чат ATL 11.13", price: 11.12, newTitle: "Чат ATL 11.12" },
+
+  { oldTitle: "Чат 10WH 11.11", price: 11.12, newTitle: "Чат 10WH 11.12" },
+  { oldTitle: "Чат 10WH 11.13", price: 11.12, newTitle: null },
+
+  { oldTitle: "Чат 10WL 11.11", price: 11.12, newTitle: null },
+  { oldTitle: "Чат 10WL 11.13", price: 11.12, newTitle: "Чат 10WL 11.12" },
+
+  { oldTitle: "Чат ATH ATL 11.11", price: 11.12, newTitle: null },
+  { oldTitle: "Чат ATH ATL 11.13", price: 11.12, newTitle: null },
+];
+
+cases.forEach(({ oldTitle, price, newTitle }) => {
+  Deno.test(`${oldTitle} + ${price}`, () =>
+    assertEquals(makeNewTitle(oldTitle, price), newTitle));
 });
